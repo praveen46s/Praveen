9113c273cadbf568abe50256f8f52eda8c9916cf
praveen46s <praveen.s4693@gmail.com>
2026-09-22 12:29:40 +0530

    Update app.py

--- New folder/app.py
+++ New folder/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 a=st.number_input("enter your number")
 b=st.number_input("enter another number")
-if-st.button("add"):
+if st.button("add"):
       st.success(a+b)
 elif st.button("subtract"):
       st.success(a-b)
